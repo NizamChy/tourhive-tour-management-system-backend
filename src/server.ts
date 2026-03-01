938c3a7563010879e867a9038bd048a48af24059
NizamChy <nizam.chy.cs@gmail.com>
2026-03-02 04:05:32 +0600

    User api created

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,6 @@ let server: Server;
 
 const startServer = async () => {
   try {
-    console.log(envVars.NODE_ENV);
     await mongoose.connect(envVars.DB_URL);
 
     console.log("Connected to DB!");
