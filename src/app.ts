504f662be0b0550557509b7df08f6face4c5e966
NizamChy <nizam.chy.cs@gmail.com>
2026-03-02 14:38:46 +0600

    not found route added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express, { Request, Response } from "express";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 import { router } from "./app/routes";
 
 const app = express();
@@ -15,5 +16,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use(globalErrorHandler);
+
+app.use(notFound);
 
 export default app;
