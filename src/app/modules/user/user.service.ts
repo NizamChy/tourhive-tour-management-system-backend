dbf6b42f359832f0819950136d9e01efc228bfef
NizamChy <nizam.chy.cs@gmail.com>
2026-03-23 23:08:52 +0600

    User routes added

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,8 +1,10 @@
 import AppError from "../../errorHelpers/AppError";
-import { IAuthProvider, IUser } from "./user.interface";
+import { IAuthProvider, IUser, Role } from "./user.interface";
 import { User } from "./user.model";
 import httpStatus from "http-status-codes";
 import bcryptjs from "bcryptjs";
+import { envVars } from "../../config/env";
+import { JwtPayload } from "jsonwebtoken";
 
 const createUser = async (payload: Partial<IUser>) => {
   const { email, password, ...rest } = payload;
@@ -16,7 +18,10 @@ const createUser = async (payload: Partial<IUser>) => {
     );
   }
 
-  const hashedPassword = await bcryptjs.hash(password as string, 10);
+  const hashedPassword = await bcryptjs.hash(
+    password as string,
+    Number(envVars.BCRYPT_SALT_ROUND),
+  );
 
   const authProvider: IAuthProvider = {
     provider: "credentials",
@@ -31,6 +36,56 @@ const createUser = async (payload: Partial<IUser>) => {
   });
 
   return user;
+};
+
+const updateUser = async (
+  userId: string,
+  payload: Partial<IUser>,
+  decodedToken: JwtPayload,
+) => {
+  const isUserExist = await User.findById(userId);
+
+  if (!isUserExist) {
+    throw new AppError(httpStatus.NOT_FOUND, "User not found");
+  }
+
+  /**
+   * email - can not be updated
+   * password - can be updated but need to hash before save
+   * role - can not be updated by normal user, only admin and super admin can update role
+   * isActive - can not be updated by normal user, only admin and super admin can update isActive status
+   * isDeleted - can not be updated by normal user, only admin and super admin can update isDeleted status
+   * promoting to super admin - only super admin can promote other user to super admin
+   */
+
+  if (payload.role) {
+    if (decodedToken.role === Role.USER || decodedToken.role === Role.GUIDE) {
+      throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
+    }
+
+    if (payload.role === Role.SUPER_ADMIN && decodedToken.role === Role.ADMIN) {
+      throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
+    }
+  }
+
+  if (payload.isActive || payload.isDeleted || payload.isVerified) {
+    if (decodedToken.role === Role.USER || decodedToken.role === Role.GUIDE) {
+      throw new AppError(httpStatus.FORBIDDEN, "You are not authorized");
+    }
+  }
+
+  if (payload.password) {
+    payload.password = await bcryptjs.hash(
+      payload.password,
+      Number(envVars.BCRYPT_SALT_ROUND),
+    );
+  }
+
+  const newUpdatedUser = await User.findByIdAndUpdate(userId, payload, {
+    new: true,
+    runValidators: true,
+  });
+  return newUpdatedUser;
 };
 
 const getAllUsers = async () => {
@@ -48,4 +103,5 @@ const getAllUsers = async () => {
 export const UserServices = {
   createUser,
   getAllUsers,
+  updateUser,
 };
